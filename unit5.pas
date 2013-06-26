1003c33d623cef1a59fa7baf9b3f07e9d285f3d8
Nick Scheynen <info@webstudions.be>
2013-06-26 01:33:41 -0700

    Aanmaken units + 3 formulieren

---
+++ unit5.pas
@@ -0,0 +1,26 @@
+unit Unit5;  // Reis boeken
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm7 = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form7: TForm7;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
