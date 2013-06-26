f6162e683dc5952a9d519ee5130953736afb1764
Stijn <ram_s_d@hotmail.com>
2013-06-26 16:53:10 +0200

    form 6
    sql

--- unit4.pas
+++ unit4.pas
@@ -5,7 +5,8 @@ unit Unit4; // Klanten wijzigen
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, unit5;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Grids, unit5, mysql55conn, sqldb;
 
 type
 
@@ -13,7 +14,12 @@ type
 
   TForm6 = class(TForm)
     Buttonreis: TButton;
+    MySQL55Connection1: TMySQL55Connection;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    StringGrid1: TStringGrid;
     procedure ButtonreisClick(Sender: TObject);
+    procedure filform;
   private
     { private declarations }
   public
@@ -34,5 +40,29 @@ begin
   form7.show;
 end;
 
+procedure TForm6.filform;
+var i:integer;
+begin
+  SQLQuery1.Active:=FALSE;
+  SQLQuery1.SQL.Clear;
+  SQLQuery1.SQL.Add('SELECT * FROM KLANTEN');
+  SQLQuery1.ExecSQL;
+  i:=1;
+  while not  SQLQuery1.EOF do begin
+    showmessage('we zitten erin');
+    stringgrid1.Cells[1,i]:=SQLquery1.FieldByName('VNAAM').AsString;
+    sqlquery1.Next;
+    inc(i);
+  end;
+
+end;
+initialization
+begin
+form6.filform;
+end;
+
 end.
+
+
+
 
